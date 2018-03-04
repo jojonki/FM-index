10a7db6acc35de052a4ea5da47379810d53d156a
jojonki <junki.ohmura@gmail.com>
2018-03-03 23:09:34 -0500

    effective sort

--- bwt.py
+++ bwt.py
@@ -18,8 +18,16 @@ class BWT:
         # print('circulant_mat', circ_mat)
         # print('sorted circulant_mat', sorted(circ_mat))
         # print('sorted_idx', sorted_idx)
-        # print('bwt', bwt)
+        # print('bwt', self.bwt)
+        self.sort_index_map = sorted(range(L), key=lambda k: self.bwt[k])
         # print('original index', org_idx)
+
+    def _fixed_sort(self, d):
+        '''
+        Effective sort by using that sort mapping is always same in BWT
+        '''
+        assert self.sort_index_map is not None
+        return [d[index] for index in self.sort_index_map]
 
     def decode(self):
         assert self.bwt is not None
@@ -27,9 +35,9 @@ class BWT:
         assert self.org_idx is not None
 
         L = len(self.text)
-        d = sorted(self.bwt)
+        d = self._fixed_sort(self.bwt)
         for _ in range(L - 1):
             for i in range(L):
                 d[i] = self.bwt[i] + d[i]
-            d = sorted(d)
+            d = self._fixed_sort(d)
         return d[self.org_idx]
